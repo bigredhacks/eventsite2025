7e5e262c33f050da325ac524dc1b8a9100adce69
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-09-20 19:26:45 -0400

    center sponsors

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
 import { Box, Card, CardMedia, Grid, Typography } from '@mui/material';
-import pinata from '../assets/pinata.png';
-import klaviyo from '../assets/klaviyo.png';
+import React from 'react';
 import ey from '../assets/ey.png';
+import klaviyo from '../assets/klaviyo.png';
+import pinata from '../assets/pinata.png';
 import scm from '../assets/scm.png';
 import sos from '../assets/stand_out_stickers.png';
 
@@ -11,18 +11,18 @@ const sponsors = [
     { name: 'Klaviyo', img: klaviyo, link: 'https://www.klaviyo.com/' },
     { name: 'EY', img: ey, link: 'https://www.ey.com/en_us' },
     { name: 'SCM', img: scm, link: 'https://www.scm-lp.com/' },
-    { name: 'Stand Out Stickers', img: sos, link: 'http://hackp.ac/mlh-StandOutStickers-hackathons' },
+    { name: 'Stand Out Stickers', img: sos, link: 'http://hackp.ac/mlh-StandOutStickers-hackathons' }
 ];
 
 const Sponsors: React.FC = () => {
     return (
         <Box>
-            <Box id="Sponsors" sx={{ paddingLeft: '100px', backgroundColor: '#FFFCF4' }}>
-                <Typography variant="h4" sx={{ fontWeight: 'bold', color: '#EA9A00', marginBottom: '20px' }}>
+            <Box id='Sponsors' sx={{ paddingLeft: '100px', backgroundColor: '#FFFCF4' }}>
+                <Typography variant='h4' sx={{ fontWeight: 'bold', color: '#EA9A00', marginBottom: '20px' }}>
                     Sponsors
                 </Typography>
             </Box>
-            <Grid container spacing={4} justifyContent="center" alignItems="stretch" paddingBottom='100px'>
+            <Grid container spacing={4} justifyContent='center' alignItems='stretch' paddingBottom='100px' paddingX={{ xs: '150px', sm: '160px', md: '50px' }}>
                 {sponsors.map((sponsor, index) => (
                     <Grid item key={index} xs={12} sm={6} md={4} lg={2}>
                         <Box
@@ -30,17 +30,12 @@ const Sponsors: React.FC = () => {
                                 display: 'flex',
                                 flexDirection: 'column',
                                 justifyContent: 'flex-end',
-                                height: '100%',
+                                height: '100%'
                             }}
                         >
-                            <Card elevation={0} sx={{ maxWidth: 'max-content' }}>
+                            <Card elevation={0}>
                                 <a href={sponsor.link} target='_blank'>
-                                    <CardMedia
-                                        component="img"
-                                        image={sponsor.img}
-                                        alt={`${sponsor.name} logo`}
-                                        sx={{ backgroundColor: '#FFFCF4' }}
-                                    />
+                                    <CardMedia component='img' image={sponsor.img} alt={`${sponsor.name} logo`} sx={{ backgroundColor: '#FFFCF4' }} />
                                 </a>
                             </Card>
                         </Box>
