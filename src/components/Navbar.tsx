6425486b3f319a498f443a94c0b929920e5bba65
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2024-09-20 11:04:27 -0400

    make navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,86 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Box, Button, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
 import React from 'react';
-import { AppBar, Toolbar, Button, Box } from '@mui/material';
 import brhlogo from '../assets/brh-logo.png';
 
 const Header: React.FC = () => {
+    const pages = ['About', 'Tracks', 'FAQ', 'Sponsors'];
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
     return (
-        <>
-            <AppBar position="static" elevation={0} sx={{ backgroundColor: '#FFFCF4', color: '#000' }}>
-                <Toolbar sx={{ justifyContent: 'space-between', width: 'calc(100vw - 225px)' }}>
-                    <Box display="flex" alignItems="center">
-                        <img src={brhlogo} alt="Big Red Hacks Logo" style={{ height: '40px', marginRight: '10px' }} />
+        <Box style={{ display: 'flex' }}>
+            <AppBar position='static' elevation={0} sx={{ background: 'none', color: 'black', width: '90%' }}>
+                <Toolbar sx={{ justifyContent: 'space-between' }}>
+                    <Box display='flex' alignItems='center'>
+                        <img src={brhlogo} alt='Big Red Hacks Logo' style={{ height: '40px', marginRight: '10px' }} />
                     </Box>
-                    <Box display="flex" alignItems="center">
-                        {['About', 'Tracks', 'FAQ', 'Sponsors'].map((item) => (
-                            <Button key={item} href={`#${item}`} color="inherit" sx={{ fontWeight: 'bold', margin: '0 10px' }}>
-                                {item}
-                            </Button>
-                        ))}
-                        <a href="https://apply.bigredhacks.com" target="_blank" rel="noreferrer">
+                    <Box display='flex' alignItems='center'>
+                        {/* Regular menu */}
+                        <Box display={{ xs: 'none', md: 'flex' }}>
+                            {pages.map(item => (
+                                <Button key={item} href={`#${item}`} color='inherit' sx={{ fontWeight: 'bold', margin: '0 10px' }}>
+                                    {item}
+                                </Button>
+                            ))}
+                        </Box>
+
+                        {/* Responsive Hamburger Menu */}
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton size='large' aria-label='account of current user' aria-controls='menu-appbar' aria-haspopup='true' onClick={handleOpenNavMenu} color='inherit'>
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id='menu-appbar'
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left'
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left'
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{ display: { xs: 'block', md: 'none' } }}
+                            >
+                                {pages.map(page => (
+                                    <MenuItem key={page} onClick={handleCloseNavMenu} component='a' href={`#${page}`}>
+                                        <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+
+                        <a href='https://apply.bigredhacks.com' target='_blank' rel='noreferrer'>
                             <Button
-                                variant="contained"
+                                variant='contained'
                                 sx={{
                                     backgroundColor: '#FA3934',
                                     fontWeight: 'bold',
                                     textTransform: 'none',
                                     marginLeft: '20px'
-                                }}>
+                                }}
+                            >
                                 Apply
                             </Button>
                         </a>
                     </Box>
                 </Toolbar>
             </AppBar>
-            <a id="mlh-trust-badge" style={{ display: 'block', maxWidth: '100px', minWidth: '60px', right: '50px', top: '0px', position: 'absolute', width: '10%' }} href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=red" target="_blank">
-                <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-red.svg" alt="Major League Hacking 2025 Hackathon Season" style={{ width: '100%' }} />
+            <a id='mlh-trust-badge' style={{ display: 'block', maxWidth: '100px', minWidth: '60px', right: '50px', top: '0px', width: '10%' }} href='https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=red' target='_blank'>
+                <img src='https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-red.svg' alt='Major League Hacking 2025 Hackathon Season' style={{ width: '100%' }} />
             </a>
-        </>
+        </Box>
     );
 };
 
